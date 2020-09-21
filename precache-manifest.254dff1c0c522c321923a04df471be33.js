7c314c316386afa75a71a46dc7f0c48b7adc7261
Jericho Thomason <jerichothomason@gmail.com>
2020-09-21 17:46:39 +0530

    Updates

---
+++ precache-manifest.254dff1c0c522c321923a04df471be33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333440f162ba2ba27ca10f21a1fe5daa",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "8ed74fb0fa2fdd8bd80a",
+    "url": "/react-web/static/css/main.ea52cdd4.chunk.css"
+  },
+  {
+    "revision": "2ea1db0931f097b0698e",
+    "url": "/react-web/static/js/2.2fc3ea41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-web/static/js/2.2fc3ea41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed74fb0fa2fdd8bd80a",
+    "url": "/react-web/static/js/main.8b891b14.chunk.js"
+  },
+  {
+    "revision": "67866d1a721f56827693",
+    "url": "/react-web/static/js/runtime-main.a485ec1d.js"
+  }
+]);
