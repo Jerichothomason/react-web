ce29916255e6d32419b380a02ba2215345507c3c
Jericho Thomason <jerichothomason@gmail.com>
2020-09-21 18:24:52 +0530

    Updates

---
+++ precache-manifest.fe65083db1912909d0ecd5e05799f98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a885fc272936a5ced2b5804b8fd11a3f",
+    "url": "/Jerichothomason/react-web/index.html"
+  },
+  {
+    "revision": "733bafe817f16883c3cd",
+    "url": "/Jerichothomason/react-web/static/css/main.ea52cdd4.chunk.css"
+  },
+  {
+    "revision": "6758040c6d29088e8d2f",
+    "url": "/Jerichothomason/react-web/static/js/2.d13cb71e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jerichothomason/react-web/static/js/2.d13cb71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733bafe817f16883c3cd",
+    "url": "/Jerichothomason/react-web/static/js/main.250a025c.chunk.js"
+  },
+  {
+    "revision": "70572fd6ced46197e5fa",
+    "url": "/Jerichothomason/react-web/static/js/runtime-main.33864fd2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jerichothomason/joel_naren_colab.git/precache-manifest.ad6298ed279402bf8b26240294754547.js"
+  "/Jerichothomason/react-web/precache-manifest.fe65083db1912909d0ecd5e05799f98a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jerichothomason/joel_naren_colab.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jerichothomason/react-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
