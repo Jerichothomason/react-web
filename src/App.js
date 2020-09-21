a518b6ce2f623e290fa82603b574be41da538005
Jericho Thomason <jerichothomason@gmail.com>
2020-09-21 18:24:15 +0530

    second try

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './App.css';
-import Tweets from './components/Tweets';
-import Top from './data/Top'
-import Main from './data/Main'
+
 
 function App() {
 
   return (
     <div>
-      <div> <Top/></div>
-      <div> <Main /></div>
+      Hhi
     </div>
   );
 }
--- src/components/bar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Bar = () =>(
-    <div>thi sis amazing grace</div>
-)
-
-export default Bar;
